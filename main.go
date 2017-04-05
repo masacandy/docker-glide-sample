ea18506e553f2d8c26cd966dd8e92d573722973a
James <masajames.1016@gmail.com>
2017-04-05 18:58:12 +0900

    cologを使う

--- main.go
+++ main.go
@@ -3,39 +3,14 @@ package main
 // Packages
 import (
 	"fmt"
-	log "github.com/cihub/seelog"
+	"log"
+	"github.com/comail/colog"
 	"net"
 	"net/http"
 	"net/http/fcgi"
-	"os"
 
 	elastic "gopkg.in/olivere/elastic.v5"
 )
-
-// ログの設定
-const logConfig = `
-<seelog type="adaptive" mininterval="200000000" maxinterval="1000000000" critmsgcount="5">
-<formats>
-    <format id="main" format="%Date(2006-01-02T15:04:05.999999999Z07:00) [%File:%FuncShort:%Line] [%LEV] %Msg%n" />
-</formats>
-<outputs formatid="main">
-    <filter levels="trace,debug,info,warn,error,critical">
-        <console />
-    </filter>
-    <filter levels="info,warn,error,critical">
-        <rollingfile filename="/tmp/log.log" type="size" maxsize="102400" maxrolls="1" />
-    </filter>
-</outputs>
-</seelog>`
-
-func initLogger() {
-	logger, err := log.LoggerFromConfigAsBytes([]byte(logConfig))
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	log.ReplaceLogger(logger)
-}
 
 func handler(resp http.ResponseWriter, req *http.Request) {
 	client, err := elastic.NewClient(elastic.SetURL("http://elasticsearch:9200"))
@@ -49,12 +24,17 @@ func handler(resp http.ResponseWriter, req *http.Request) {
 	}
 	fmt.Fprintf(resp, "<h1>%s</h1>", esversion)
 
-	log.Info(esversion)
+	log.Printf(esversion)
 }
 
 func main() {
-	initLogger()
-	defer log.Flush()
+   	colog.SetDefaultLevel(colog.LDebug)
+	colog.SetMinLevel(colog.LTrace)
+	colog.SetFormatter(&colog.StdFormatter{
+		Colors: true,
+		Flag:   log.Ldate | log.Ltime | log.Lshortfile,
+	})
+	colog.Register()
 
 	listener, err := net.Listen("tcp", ":9000")
 	if err != nil {
